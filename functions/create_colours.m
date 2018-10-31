886e270e4bfa35f03da5fa5942be041b31ce7ad0
Guillaume Tousignant <moi_guillaume@hotmail.com>
2018-10-31 18:53:57 -0400

    added colours

---
+++ functions/create_colours.m
@@ -0,0 +1,18 @@
+colours = struct();
+
+colours.yellow = [0.98, 1, 0.9];
+colours.purple = [0.98, 0.7, 0.85];
+colours.green = [0.8, 0.95, 0.6];
+colours.white = [1, 1, 1];
+colours.black = [0, 0, 0];
+colours.grey1 = [0.5, 0.5, 0.5];
+colours.grey2 = [0.75, 0.75, 0.75];
+colours.blue = [0.1, 0.4, 0.8];
+colours.turquoise = [0.6, 0.95, 0.8];
+colours.watercolour = [0.6, 0.9, 1];
+colours.red = [0.9568627450980392, 0.2784313725490196, 0.2784313725490196];
+colours.gemcolor = [0 0.9 1];
+colours.orange = [1, 0.6039, 0];
+colours.teal = [0.1529, 0.6549, 0.8471];
+
+save colours_mat colours
