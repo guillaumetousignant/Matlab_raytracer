6654c91647fa08675df35bf66a42ece7e0f57c20
Guillaume Tousignant <moi_guillaume@hotmail.com>
2018-10-31 16:18:22 -0400

    updated 3D raytrace functions

--- cameras/cam_3D_aperture.m
+++ cameras/cam_3D_aperture.m
@@ -68,48 +68,8 @@ methods
     end
 
     function raytrace(obj, scene)
-        
-        tot_subpix = obj.subpix(1) * obj.subpix(2);
-        pixel_span_y = obj.pixel_span(1);
-        pixel_span_x = obj.pixel_span(2);
-        res_y = obj.resolution(1);
-        res_x = obj.resolution(2);
-        subpix_y = obj.subpix(1);
-        subpix_x = obj.subpix(2);
-        is_in = obj.material;
-        origin1 = obj.origin;
-        direction_sph1 = to_sph(obj.direction);
-
-        output = zeros(res_y, res_x, 3); %%% for parfor normal rendering
-        
-        parfor j = 1:res_y
-            outline = zeros(1, res_x, 3);
-            for i = 1:res_x
-                col = [0, 0, 0];
-                pix_vec_sph = direction_sph1 + [0, (j-res_y/2-0.5)*pixel_span_y, (i-res_x/2-0.5)*-pixel_span_x]; 
-
-                for k = 1:subpix_y
-                    for l = 1:subpix_x
-                        ray_vec = to_xyz(pix_vec_sph + [0, pixel_span_y*(k/subpix_y-0.5), -pixel_span_x*(l/subpix_x-0.5)]);
-                        
-                        aray = ray(origin1, ray_vec, [0, 0, 0], [1, 1, 1], is_in);
-                        aray.raycast(scene, obj);
-                        col = col + aray.colour;
-                    end
-                end
-        
-                col = col/tot_subpix;
-                outline(1, i, :) = col;
-            end
-            %obj.image.setline(outline, j);
-            %obj.image.updateline(outline, j); % possibly not working as intended
-
-            output(j, :, :) = outline; %%% for parfor normal rendering
-            
-            %%% REMOVE
-            %fprintf([num2str(round(100*j/res_y)), '%% done!\n']);
-        end
-        obj.image.set(output); %%% for parfor normal rendering
+        obj.camera_L.raytrace(scene);
+        obj.camera_R.raytrace(scene);
     end  
 end
 end
