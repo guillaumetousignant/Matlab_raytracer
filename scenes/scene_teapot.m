23182390f6d7eae6f5d39487e1628a8f85d75d77
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-01-21 19:47:50 -0500

    new scene structure

--- scenes/scene_teapot.m
+++ scenes/scene_teapot.m
@@ -1,24 +1,11 @@
 %% Scene
 
 % Colours
-yellow = [0.98, 1, 0.9];
-purple = [0.98, 0.7, 0.85];
-green = [0.8, 0.95, 0.6];
-white = [1, 1, 1];
-black = [0, 0, 0];
-grey1 = [0.5, 0.5, 0.5];
-grey2 = [0.75, 0.75, 0.75];
-blue = [0.1, 0.4, 0.8];
-turquoise = [0.6, 0.95, 0.8];
-watercolour = [0.6, 0.9, 1];
-red = [0.9568627450980392, 0.2784313725490196, 0.2784313725490196];
-gemcolor = [0 0.9 1];
-orange = [1, 0.6039, 0];
-teal = [0.1529, 0.6549, 0.8471];
+load colours_mat colours
 
 % General stuff
 neutralmatrix = transformmatrix(); % Should never be changed, used for triangles
-air = refractive(black, white, 1.001, struct('ind', 0), nonabsorber()); %%% CHECK generate_scene is 10x slower when putting [] as is_in, this is a workaround
+air = refractive(colours.black, colours.white, 1.001, struct('ind', 0), nonabsorber()); %%% CHECK generate_scene is 10x slower when putting [] as is_in, this is a workaround
 
 
 scenename = 'teapot';
@@ -28,11 +15,11 @@ fprintf('\nScene building\n');
 tic
 
 % Materials
-difgrey = diffuse(black, grey1, 1);
-red_metal = reflective_fuzz(black, red, 2, 1);
-coating = reflective(black, white);
+difgrey = diffuse(colours.black, colours.grey1, 1);
+red_metal = reflective_fuzz(colours.black, colours.red, 2, 1);
+coating = reflective(colours.black, colours.white);
 teapotmat = fresnelmix(red_metal, coating, 1.5);
-glass = reflective_refractive(black, white, 1.5, air, absorber(black, white, 100, 100));
+glass = reflective_refractive(colours.black, colours.white, 1.5, air, absorber(colours.black, colours.white, 100, 100));
 
 % Objects
 planegrey3 = triangle(difgrey, [-2, 4, -0.5; -2, 0, -0.5; 2, 0, -0.5], [], [], neutralmatrix);
