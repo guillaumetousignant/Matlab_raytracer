6bed2b2baa4dbedadfece7e015e14ecd0e615332
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-01-28 16:36:46 -0500

    fixed 3D overlap bug in filename exist check

--- functions/next_filename.m
+++ functions/next_filename.m
@@ -10,8 +10,11 @@ function new_filename = next_filename(prefix)
 
     index = 1;
     new_filename = sprintf('%s%03i%s', name, index, extension);
+    new_filename_L = sprintf('%s%03i_L%s', name, index, extension);
+    new_filename_R = sprintf('%s%03i_R%s', name, index, extension);
+    new_filename_S = sprintf('%s%03i_S%s', name, index, extension);
 
-    while exist(new_filename, 'file') == 2
+    while (exist(new_filename, 'file') == 2) || (exist(new_filename_L, 'file') == 2) || (exist(new_filename_R, 'file') == 2) || (exist(new_filename_S, 'file') == 2)
         index = index + 1;
         new_filename = sprintf('%s%03i%s', name, index, extension);
     end
