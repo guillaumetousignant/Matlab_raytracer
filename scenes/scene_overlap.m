65aa9d5866f588b617dcfb3060018b718fd8bfb5
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-03 15:02:46 -0500

    new xml struct format

--- scenes/scene_overlap.m
+++ scenes/scene_overlap.m
@@ -1,77 +1,181 @@
 %% Scene
+scene_name = 'overlap';
+scene_struct = struct('scene', struct('name', scene_name));
 
-% Colours
-load colours_mat colours
+%% Scattering
+scatterer_cell = cell(3, 1);
 
-% General stuff
-neutralmatrix = transformmatrix(); % Should never be changed, used for triangles
-air = refractive(colours.black, colours.white, 1.001, 0, nonabsorber()); %%% CHECK generate_scene is 10x slower when putting [] as is_in, this is a workaround
+scatterer_cell{1}.name = 'red_absorber';
+scatterer_cell{1}.type = 'absorber';
+scatterer_cell{1}.emission = 'black';
+scatterer_cell{1}.colour = 'red';
+scatterer_cell{1}.emission_distance = 1000;
+scatterer_cell{1}.absorption_distance = 2;
 
+scatterer_cell{2}.name = 'blue_absorber';
+scatterer_cell{2}.type = 'absorber';
+scatterer_cell{2}.emission = 'black';
+scatterer_cell{2}.colour = 'watercolour';
+scatterer_cell{2}.emission_distance = 1000;
+scatterer_cell{2}.absorption_distance = 2;
 
-scenename = 'overlap';
-filename = next_filename(['.', filesep, 'images', filesep, scenename, '.png']);
-%filename = ['.', filesep, 'images', filesep, 'output', '.png'];
+scatterer_cell{3}.name = 'air_absorber';
+scatterer_cell{3}.type = 'nonabsorber';
 
-fprintf('\nScene name: %s\n', scenename);
-fprintf('\nScene building\n');
-tic
+scene_struct.scene.scatterers.scatterer = scatterer_cell;
 
-% Materials
-difgrey = diffuse(colours.black, colours.grey1, 1);
-glass = reflective_refractive_fuzz(colours.black, colours.white, 1.5, 30, 2, 0.05, absorber(colours.black, colours.red, 1000, 2));
-glass2 = reflective_refractive_fuzz(colours.black, colours.white, 1.33, 20, 1, 0.05, absorber(colours.black, colours.watercolour, 1000, 2));
+%% Materials
+material_cell = cell(4, 1);
 
-% Objects
-planegrey1 = triangle(difgrey, [-1000, 1000, -1; -1000, -1000, -1; 1000, -1000, -1], [], [], neutralmatrix);
-planegrey2 = triangle(difgrey, [-1000, 1000, -1; 1000, -1000, -1; 1000, 1000, -1], [], [], neutralmatrix);
+material_cell{1}.name = 'difgrey';
+material_cell{1}.type = 'diffuse';
+material_cell{1}.emission = 'black'; % can also be array
+material_cell{1}.colour = 'grey1'; % can also be array
+material_cell{1}.roughness = 1;
 
-sphere1 = sphere(glass, transformmatrix());
-sphere1.transformation.translate([-0.4, 3, 0]);
-sphere1.transformation.uniformscale(1);
-sphere2 = sphere(glass2, transformmatrix());
-sphere2.transformation.translate([0.7, 3, 0]);
-sphere2.transformation.uniformscale(1);
+material_cell{2}.name = 'glass';
+material_cell{2}.type = 'reflective_refractive_fuzz';
+material_cell{2}.emission = 'black'; % can also be array
+material_cell{2}.colour = 'white'; % can also be array
+material_cell{2}.ind = 1.5;
+material_cell{2}.priority = 30;
+material_cell{2}.order = 2;
+material_cell{2}.diffusivity = 0.05;
+material_cell{2}.scattering_fn = 1; % can be index or name
 
-ascene = scene(sphere1, sphere2, planegrey1, planegrey2);
+material_cell{3}.name = 'glass2';
+material_cell{3}.type = 'reflective_refractive_fuzz';
+material_cell{3}.emission = 'black'; % can also be array
+material_cell{3}.colour = 'white'; % can also be array
+material_cell{3}.ind = 1.33;
+material_cell{3}.priority = 20;
+material_cell{3}.order = 1;
+material_cell{3}.diffusivity = 0.05;
+material_cell{3}.scattering_fn = 2; % can be index or name
 
-toc
+material_cell{4}.name = 'air';
+material_cell{4}.type = 'refractive';
+material_cell{4}.emission = 'black'; % can also be array
+material_cell{4}.colour = 'white'; % can also be array
+material_cell{4}.ind = 1.001;
+material_cell{4}.priority = 0;
+material_cell{4}.scattering_fn = 3; % can be index or name
 
-fprintf('\nScene updating\n');
-tic
-ascene.update;
-toc
+scene_struct.scene.materials.material = material_cell;
 
-fprintf('\nAcceleration structure building\n');
-tic
-ascene.buildacc;
-toc
+%% Transform matrices
+transform_cell = cell(1, 1);
 
-%save scene.mat ascene
+transform_cell{1}.name = 'neutralmatrix';
+transform_cell{1}.value = []; % fill if is empty
+
+scene_struct.scene.transform_matrices.transform_matrix = transform_cell;
+
+%% Objects
+object_cell = cell(4, 1);
+
+object_cell{1}.name = 'planegrey1';
+object_cell{1}.type = 'triangle';
+object_cell{1}.material = 1; % can also be name
+object_cell{1}.points = [-1000, 1000, -1; -1000, -1000, -1; 1000, -1000, -1];
+object_cell{1}.normals = [];
+object_cell{1}.texture_coordinates = [];
+object_cell{1}.transform_matrix = 1; % if not empty, search for right matrix, if empty, create.
+
+object_cell{2}.name = 'planegrey2';
+object_cell{2}.type = 'triangle';
+object_cell{2}.material = 1; % can also be name
+object_cell{2}.points = [-1000, 1000, -1; 1000, -1000, -1; 1000, 1000, -1];
+object_cell{2}.normals = [];
+object_cell{2}.texture_coordinates = [];
+object_cell{2}.transform_matrix = 1; % if not empty, search for right matrix, if empty, create.
+
+object_cell{3}.name = 'sphere1';
+object_cell{3}.type = 'sphere';
+object_cell{3}.material = 2; % can also be name
+object_cell{3}.transform_matrix = []; % if not empty, search for right matrix, if empty, create.
+transformation_pre_cell = cell(2, 1);
+transformation_pre_cell{1}.type = 'translation';
+transformation_pre_cell{1}.value = [-0.4, 3, 0];
+transformation_pre_cell{2}.type = 'uniformscale';
+transformation_pre_cell{2}.value = 1;
+object_cell{3}.transformations_pre.transformation_pre = transformation_pre_cell;
+
+object_cell{4}.name = 'sphere2';
+object_cell{4}.type = 'sphere';
+object_cell{4}.material = 3; % can also be name
+object_cell{4}.transform_matrix = []; % if not empty, search for right matrix, if empty, create.
+transformation_pre_cell = cell(2, 1);
+transformation_pre_cell{1}.type = 'translation';
+transformation_pre_cell{1}.value = [0.7, 3, 0];
+transformation_pre_cell{2}.type = 'uniformscale';
+transformation_pre_cell{2}.value = 1;
+object_cell{4}.transformations_pre.transformation_pre = transformation_pre_cell;
+
+scene_struct.scene.objects.object = object_cell;
+
+%% Directional lights
+directional_light_cell = cell(1, 1);
+
+directional_light_cell{1}.name = 'sun';
+directional_light_cell{1}.colour = [2.5, 2.5, 2] * 2;
+directional_light_cell{1}.transform_matrix = [];
+transformation_pre_cell = cell(3, 1);
+transformation_pre_cell{1}.type = 'uniformscale';
+transformation_pre_cell{1}.value = 0.95;
+transformation_pre_cell{2}.type = 'rotatez';
+transformation_pre_cell{2}.value = -pi/4;
+transformation_pre_cell{3}.type = 'rotatex';
+transformation_pre_cell{3}.value = -3 * pi/8;
+directional_light_cell{1}.transformations_pre.transformation_pre = transformation_pre_cell;
+
+scene_struct.scene.directional_lights.directional_light = directional_light_cell;
+
+%% Skyboxes
+skybox_cell = cell(1, 1);
+
+skybox_cell{1}.name = 'day';
+skybox_cell{1}.type = 'skybox_flat_sun';
+skybox_cell{1}.background = [0.8040, 0.8825, 0.9765];
+skybox_cell{1}.lights = 1; % can be array of indices, or cell array of names. Maybe should be another struct?
+
+scene_struct.scene.skyboxes.skybox = skybox_cell;
+
+%% Image buffers
+res_x = 1800;
+res_y = 1200;
+imgbuffer_cell = cell(1, 1);
+
+imgbuffer_cell{1}.name = 'buffer1';
+imgbuffer_cell{1}.type = 'imgbuffer';
+imgbuffer_cell{1}.resx = res_x;
+imgbuffer_cell{1}.resy = res_y;
+
+scene_struct.scene.imgbuffers.imgbuffer = imgbuffer_cell;
 
 %% Camera
-upvector = [1, 0, 1];
-upvector = upvector/norm(upvector);
-camera = generate_camera([1800, 1200], 'bg', 'beach', 'type', 'cam', 'file', filename, 'material', air); 
+aspect_ratio = res_x/res_y;
+fov(2) = 80 * pi/180;
+fov(1) = fov(2)/aspect_ratio;
 
-%camera.transformation.rotatez(-pi/6);
-%camera.transformation.translate([-1, -2, 0]);
-%camera.transformation.rotatex(-pi/16);
+camera_cell = cell(1, 1);
 
-camera.update;
-camera.update; % second time to kill blur
+camera_cell{1}.name = 'camera1';
+camera_cell{1}.type = 'cam';
+camera_cell{1}.transformation_matrix = [];
+camera_cell{1}.filename = []; % if is empty, use next available with scene name
+camera_cell{1}.up = [0, 0, 1];
+camera_cell{1}.fov = fov;
+camera_cell{1}.subpix = [1, 1];
+camera_cell{1}.imgbuffer = 1; % can be index or name
+camera_cell{1}.material = 4; % can be index or name
+camera_cell{1}.skybox = 1; % can be index or name
+camera_cell{1}.max_bounces = 8;
+camera_cell{1}.gammaind = 1;
+camera_cell{1}.rendermode = 'accumulation';
+camera_cell{1}.n_iter = inf;
 
-scene_struct = struct();
-scene_struct.name = 'overlap';
+scene_struct.scene.cameras.camera = camera_cell;
 
-scene_struct.materials = cell(3, 1);
-scene_struct.materials{1} = struct('name', 'dirgrey', 'type', 'diffuse', 'emission', 'black', 'colour', 'grey1', 'roughness', 1);
-absorber1 = struct('name', 'red_absorber', 'type', 'absorber', 'emission', 'black', 'colour', 'red', 'emission_distance', 1000, 'absorption_distance', 2);
-scene_struct.materials{2} = struct('name', 'glass', 'type', 'reflective_refractive_fuzz', 'emission', 'black', 'colour', 'white', 'ind', 1.5, 'priority', 30, 'order', 2, 'diffusivity', 0.05, 'scattering_fn', absorber1);
-absorber2 = struct('name', 'blue_absorber', 'type', 'absorber', 'emission', 'black', 'colour', 'watercolour', 'emission_distance', 1000, 'absorption_distance', 2);
-scene_struct.materials{3} = struct('name', 'glass2', 'type', 'reflective_refractive_fuzz', 'emission', 'black', 'colour', 'white', 'ind', 1.33, 'priority', 20, 'order', 1, 'diffusivity', 0.05, 'scattering_fn', absorber2);
-
-scene_struct.objects = cell(4, 1);
-
-scene_struct.camera = struct();
-
-camera.accumulate(ascene);
+%% Output
+struct2xml(scene_struct, [scene_name, '.xml']);
