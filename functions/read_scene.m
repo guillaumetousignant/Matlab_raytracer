fbcc7aa2d4794213e190a0474787b5ed806b45f2
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-04 14:39:36 -0500

    updated read_scene

--- functions/read_scene.m
+++ functions/read_scene.m
@@ -10,11 +10,11 @@ function read_scene(filename, varargin)
     filename = next_filename(['.', filesep, 'images', filesep, scenename, '.png']);
 
     objects = {};
-    directional_lights = {};
     skyboxes = {};
     imgbuffers = {};
     cameras = {};
 
+    %% Creation
     if isfield(s.scene, 'transform_matrices')
         n_transform_matrices = size(s.scene.transform_matrices.transform_matrix, 1);
         transform_matrices = cell(n_transform_matrices, 1);
@@ -55,42 +55,8 @@ function read_scene(filename, varargin)
                 case 'nonabsorber'
                     scatterers{i, 1} = nonabsorber();
                 case 'portal_scatterer'
-                    value = temp.transform_matrix;
-                    [value_num, status] = str2num(value);
-                    if status
-                        if isnan(value_num)
-                            transform_matrix = transformmatrix();
-                        else
-                            transform_matrix = transform_matrices{value_num};
-                        end
-                    else
-                        for j = 1:n_transform_matrices
-                            if strcmpi(s.scene.transform_matrices.transform_matrix{j, 1}.Attributes.name, transform)
-                                transform_matrix = transform_matrices{j, 1};
-                                break
-                            end
-                        end
-                    end
-
-                    value = temp.is_in;
-                    [value_num, status] = str2num(value);
-                    if status
-                        scatterers_medium_list{i, 1} = value_num;
-                    else
-                        value = strsplit(value, {',', ';'});
-                        index = zeros(1, length(value));
-                        for j = 1:length(value)
-                            value_temp = strtrim(value{j});
-                            for k = 1:size(s.scene.materials.material, 1)
-                                if strcmpi(s.scene.materials.material{k, 1}.Attributes.name, value_temp)
-                                    index(1, j) = k;
-                                    break
-                                end
-                            end
-                        end
-                        scatterers_medium_list{i, 1} = index;
-                    end
-
+                    transform_matrix = get_transform_matrix(temp.transform_matrix);
+                    scatterers_medium_list{i, 1} = get_is_in(temp.is_in);
                     scatterers{i, 1} = portal_scatterer(transform_matrix, get_value(temp.scattering_distance), []);
                 case 'scatterer_exp'
                     scatterers{i, 1} = scatterer_exp(get_colour(temp.emission), get_colour(temp.colour), get_value(temp.emission_distance), get_value(temp.absorption_distance), get_value(temp.scattering_distance), get_value(temp.order), get_value(temp.scattering_angle));
@@ -123,64 +89,36 @@ function read_scene(filename, varargin)
                 case 'diffuse'
                     materials{i, 1} = diffuse(get_colour(temp.emission), get_colour(temp.colour), get_value(temp.roughness));
                 case 'fresnelmix_in'
-                    materials{i, 1} = 
+                    materials_mix_list(i, :) = get_materials(temp.material_refracted, temp.material_reflected);
+                    materials{i, 1} = fresnelmix_in([], [], get_value(temp.ind));
                 case 'fresnelmix'
-                    materials{i, 1} = 
+                    materials_mix_list(i, :) = get_materials(temp.material_refracted, temp.material_reflected);
+                    materials{i, 1} = fresnelmix([], [], get_value(temp.ind));
                 case 'normal_material'
                     materials{i, 1} = normal_material();
                 case 'portal_refractive'
                     materials{i, 1} = diffuse([0, 0, 0], [0.5 0.5 0.5], 1);
                     warning('read_scene:portal_refractiveNotImplemented', 'Portal_refractive not implemented, ignoring.');
                 case 'portal'
-                    value = temp.transform_matrix;
-                    [value_num, status] = str2num(value);
-                    if status
-                        if isnan(value_num)
-                            transform_matrix = transformmatrix();
-                        else
-                            transform_matrix = transform_matrices{value_num};
-                        end
-                    else
-                        for j = 1:n_transform_matrices
-                            if strcmpi(s.scene.transform_matrices.transform_matrix{j, 1}.Attributes.name, transform)
-                                transform_matrix = transform_matrices{j, 1};
-                                break
-                            end
-                        end
-                    end
-
-                    value = temp.is_in;
-                    [value_num, status] = str2num(value);
-                    if status
-                        materials_medium_list{i, 1} = value_num;
-                    else
-                        value = strsplit(value, {',', ';'});
-                        index = zeros(1, length(value));
-                        for j = 1:length(value)
-                            value_temp = strtrim(value{j});
-                            for k = 1:size(s.scene.materials.material, 1)
-                                if strcmpi(s.scene.materials.material{k, 1}.Attributes.name, value_temp)
-                                    index(1, j) = k;
-                                    break
-                                end
-                            end
-                        end
-                        materials_medium_list{i, 1} = index;
-                    end
-
+                    transform_matrix = get_transform_matrix(temp.transform_matrix);
+                    materials_medium_list{i, 1} = get_is_in(temp.is_in);
                     materials{i, 1} = portal(transform_matrix, []);
                 case 'randommix_in'
-                    materials{i, 1} = 
+                    materials_mix_list(i, :) = get_materials(temp.material_refracted, temp.material_reflected);
+                    materials{i, 1} = randommix_in([], [], get_value(temp.ratio));
                 case 'randommix'
-                    materials{i, 1} = 
+                    materials_mix_list(i, :) = get_materials(temp.material_refracted, temp.material_reflected);
+                    materials{i, 1} = randommix([], [], get_value(temp.ratio));
                 case 'reflective_fuzz'
-                    materials{i, 1} = 
+                    materials{i, 1} = reflective_fuzz(get_colour(temp.emission), get_colour(temp.colour), get_value(temp.order), get_value(temp.diffusivity));
                 case 'reflective_refractive_fuzz'
-                    materials{i, 1} = 
+                    scattering_fn = get_scattering_fn(temp.scattering_fn);
+                    materials{i, 1} = reflective_refractivefuzz(get_colour(temp.emission), get_colour(temp.colour), get_value(temp.ind), get_value(temp.priority), get_value(temp.order), get_value(temp.diffusivity), scattering_fn);
                 case 'reflective_refractive'
-                    materials{i, 1} = 
+                    scattering_fn = get_scattering_fn(temp.scattering_fn);
+                    materials{i, 1} = reflective_refractive(get_colour(temp.emission), get_colour(temp.colour), get_value(temp.ind), get_value(temp.priority), scattering_fn);
                 case 'reflective'
-                    materials{i, 1} = 
+                    materials{i, 1} = reflective(get_colour(temp.emission), get_colour(temp.colour));
                 case 'refractive_fuzz'
                     scattering_fn = get_scattering_fn(temp.scattering_fn);
                     materials{i, 1} = refractive_fuzz(get_colour(temp.emission), get_colour(temp.colour), get_colour(temp.ind), get_colour(temp.priority), get_value(temp.ind), get_value(temp.diffusivity), scattering_fn);
@@ -199,12 +137,28 @@ function read_scene(filename, varargin)
         materials_medium_list = {};
     end
 
+    if isfield(s.scene, 'directional_lights')
+        n_directional_lights = size(s.scene.directional_lights.directional_light, 1);
+        directional_lights = cell(n_directional_lights, 1);
+
+        for i = 1:n_directional_lights
+            temp = s.scene.directional_lights.directional_light{i, 1}.Attributes;
+            transform_matrix = get_transform_matrix(temp.transform_matrix);
+            directional_lights{i, 1} = directional_light(get_colour(temp.colour), transform_matrix);        
+        end
+    else
+        n_directional_lights = 0;
+        directional_lights = {};
+    end
 
 
+
+    %% Fixes
     % Materials mix fix
     for i = 1:n_materials
         if ~isempty(materials_mix_list{i, 1})
-            
+            materials{i, 1}.material_refracted = materials{materials_mix_list{i, 1}, 1};
+            materials{i, 1}.material_reflected = materials{materials_mix_list{i, 2}, 1};
         end
     end
 
@@ -231,6 +185,26 @@ function read_scene(filename, varargin)
             scatterers{i, 1}.is_in = is_in;
         end
     end
+
+    %% Updating
+    for i = 1:n_directional_lights
+        temp = s.scene.directional_lights.directional_light{i, 1};
+        if isfield(temp, 'transformations_pre')
+            n_transforms = length(temp.transformations_pre.transformation_pre);
+            for j = 1:n_transforms
+                apply_transformation(directional_lights{i, 1}, temp.transformations_pre.transformation_pre{j, 1}.Attributes);
+            end
+        end        
+    end
+
+
+
+
+
+
+
+
+
 
     function output_colour = get_colour(input_colour)
         [output_colour, colour_status] = str2num(input_colour);
@@ -262,6 +236,84 @@ function read_scene(filename, varargin)
             end
         end
     end
+
+    function output_materials = get_materials(material_refracted, material_reflected)
+        output_materials = cell(1, 2);
+
+        input_material = material_refracted;
+        [input_material_num, status] = str2num(input_material);
+        if status
+            output_materials{1, 1} = materials{input_material_num};
+        else
+            index = 0;
+            for j2 = 1:size(s.scene.materials.material, 1)
+                if strcmpi(s.scene.materials.material{j2, 1}.Attributes.name, input_material)
+                    output_materials{1, 1} = materials{j2};
+                    break
+                end
+            end
+            if ~index
+                output_materials{1, 1} = diffuse([0, 0, 0], [0.5, 0.5, 0.5], 1);
+                warning('read_scene:materialNotFound', ['Material "', input_material, '" not found, ignoring.']);
+            end
+        end
+
+        input_material = material_reflected;
+        [input_material_num, status] = str2num(input_material);
+        if status
+            output_materials{1, 2} = materials{input_material_num};
+        else
+            index = 0;
+            for j2 = 1:size(s.scene.materials.material, 1)
+                if strcmpi(s.scene.materials.material{j2, 1}.Attributes.name, input_material)
+                    output_materials{1, 2} = materials{j2};
+                    break
+                end
+            end
+            if ~index
+                output_materials{1, 2} = diffuse([0, 0, 0], [0.5, 0.5, 0.5], 1);
+                warning('read_scene:materialNotFound', ['Material "', input_material, '" not found, ignoring.']);
+            end
+        end
+    end
+
+    function transform_matrix_output = get_transform_matrix(transform_matrix_input)
+        [transform_matrix_input_num, status] = str2num(transform_matrix_input);
+        if status
+            if isnan(transform_matrix_input_num)
+                transform_matrix_output = transformmatrix();
+            else
+                transform_matrix_output = transform_matrices{transform_matrix_input_num};
+            end
+        else
+            for j3 = 1:n_transform_matrices
+                if strcmpi(s.scene.transform_matrices.transform_matrix{j3, 1}.Attributes.name, transform)
+                    transform_matrix_output = transform_matrices{j3, 1};
+                    break
+                end
+            end
+        end
+    end
+
+    function is_in_output = get_is_in(is_in_input)
+        [value_num, status] = str2num(is_in_input);
+        if status
+            is_in_output = value_num;
+        else
+            is_in_input = strsplit(is_in_input, {',', ';'});
+            index = zeros(1, length(is_in_input));
+            for j4 = 1:length(is_in_input)
+                value_temp = strtrim(is_in_input{j4});
+                for k1 = 1:size(s.scene.materials.material, 1)
+                    if strcmpi(s.scene.materials.material{k1, 1}.Attributes.name, value_temp)
+                        index(1, j4) = k1;
+                        break
+                    end
+                end
+            end
+            is_in_output = index;
+        end
+    end
 end
 
 function output_value = get_value(input_value)
@@ -269,5 +321,29 @@ function output_value = get_value(input_value)
     if ~value_status
         output_value = 1;
         warning('read_scene:unknownValue', ['Unknown value "', input_value, '", ignoring.']);
+    end
+end
+
+function apply_transformation(object, transform)
+    value = get_value(transform);
+    switch lower(transform.type)
+        case 'rotatexaxis'
+        case 'rotateyaxis'
+        case 'rotatezaxis'
+        case 'rotatex'
+        case 'rotatey'
+        case 'rotatez'
+        case 'rotateaxis'
+        case 'rotate'
+        case 'translate'
+        case 'scaleaxis'
+        case 'uniformscaleaxis'
+        case 'scale'
+        case 'uniformscale'
+        case 'reflect'
+        case 'shear'
+        case 'transpose'
+        case 'invert'
+        case 'neg'
     end
 end
