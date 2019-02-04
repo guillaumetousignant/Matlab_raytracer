3595fcc84d6ebf47c7b4d8bb523e25cbbd565313
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-04 14:40:04 -0500

    updated map format

--- scenes/scene_zombie_teapot.m
+++ scenes/scene_zombie_teapot.m
@@ -1,6 +1,7 @@
 %% Scene
 
 % Colours
+load colours_mat colours
 yellow = [0.98, 1, 0.9];
 purple = [0.98, 0.7, 0.85];
 green = [0.8, 0.95, 0.6];
@@ -18,26 +19,31 @@ teal = [0.1529, 0.6549, 0.8471];
 
 % General stuff
 neutralmatrix = transformmatrix(); % Should never be changed, used for triangles
-air = refractive(black, white, 1.001, struct('ind', 0), nonabsorber()); %%% CHECK generate_scene is 10x slower when putting [] as is_in, this is a workaround
+air = refractive(colours.black, colours.white, 1.001, 0, nonabsorber()); %%% CHECK generate_scene is 10x slower when putting [] as is_in, this is a workaround
 
 
-scenename = 'teapot';
+scenename = 'zombie_teapot';
 
 fprintf('\nScene name: %s\n', scenename);
 fprintf('\nScene building\n');
 tic
 
 % Materials
-difgrey = diffuse(black, grey1, 1);
-red_metal = reflective_fuzz(black, red, 2, 1);
-green_goo = reflective_refractive(black, white, 1.5, air, scatterer_exp(black, green, 0.05, 0.05, 0.01, 1, 1));
-coating = reflective(black, white);
+difgrey = diffuse(colours.black, colours.grey1, 1);
+red_metal = reflective_fuzz(colours.black, colours.red, 2, 1);
+green_goo = reflective_refractive(colours.black, colours.white, 1.5, 10, scatterer_exp(colours.black, colours.green, 0.05, 0.05, 0.05, 1, 1)); % 0.01 scattering distance is good
+coating = reflective(colours.black, colours.white);
 zombiemat = fresnelmix(red_metal, coating, 1.5);
-glass = reflective_refractive(black, white, 1.5, air, nonabsorber());
+glass = reflective_refractive(colours.black, colours.white, 1.5, air, nonabsorber());
+diflight = diffuse(colours.white * 8, colours.white, 1);
 
 % Objects
 planegrey3 = triangle(difgrey, [-2, 4, -0.5; -2, 0, -0.5; 2, 0, -0.5], [], [], neutralmatrix);
 planegrey4 = triangle(difgrey, [-2, 4, -0.5; 2, 0, -0.5; 2, 4, -0.5], [], [], neutralmatrix);
+
+light = sphere(diflight, transformmatrix());
+light.transformation.uniformscale(0.25);
+light.transformation.translate([0, 2, 0.5]);
 
 zombie = mesh(mesh_geometry('.\assets\Zombie_Beast4_test.obj'), green_goo, transformmatrix());
 zombie.transformation.rotatex(pi/2);
@@ -47,7 +53,7 @@ zombie.transformation.translate([0, 1.5, -0.53]);
 
 zombie.update;
 
-ascene = scene(planegrey3, planegrey4);
+ascene = scene(planegrey3, planegrey4, light);
 ascene.addmesh(zombie);
 
 toc
@@ -65,7 +71,7 @@ toc
 %save scene.mat ascene
 
 %% Camera
-camera = generate_camera([1800, 1200], 'bg', 'day', 'subpix', 1, 'type', 'aperture', 'focallength', 1.5, 'maxbounces', 32); 
+camera = generate_camera([600, 400], 'bg', 'night', 'subpix', 1, 'type', 'aperture', 'focallength', 1.5, 'maxbounces', 32, 'material', air); 
 
 %camera.transformation.rotatez(-pi/6);
 %camera.transformation.translate([-1, -2, 0]);
@@ -73,3 +79,5 @@ camera = generate_camera([1800, 1200], 'bg', 'day', 'subpix', 1, 'type', 'apertu
 
 camera.update;
 camera.update; % second time to kill blur
+
+camera.accumulate(ascene);
