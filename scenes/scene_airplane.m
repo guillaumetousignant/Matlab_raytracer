848345f368b13b04c9b98866c5c9745b66161d08
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-10 18:06:41 -0500

    added aggregate material to airplane scene

--- scenes/scene_airplane.m
+++ scenes/scene_airplane.m
@@ -6,11 +6,39 @@ scene_struct.scene.Attributes.name = scene_name;
 scene_struct.scene.Attributes.mesh_list = 'airplane';
 
 %% Textures
-texture_cell = cell(1, 1);
+texture_cell = cell(8, 1);
 
-texture_cell{1}.Attributes.name = 'airplane_texture';
+texture_cell{1}.Attributes.name = 'b737_800_2_T';
 texture_cell{1}.Attributes.type = 'texture';
-texture_cell{1}.Attributes.filename = '.\assets\A380.png';
+texture_cell{1}.Attributes.filename = '.\assets\A380\b737_800_2_T.tga';
+
+texture_cell{2}.Attributes.name = 'a380_01';
+texture_cell{2}.Attributes.type = 'texture';
+texture_cell{2}.Attributes.filename = '.\assets\A380\a380_01.tga';
+
+texture_cell{3}.Attributes.name = 'A380_mw';
+texture_cell{3}.Attributes.type = 'texture';
+texture_cell{3}.Attributes.filename = '.\assets\A380\A380_mw.tga';
+
+texture_cell{4}.Attributes.name = 'A380_L';
+texture_cell{4}.Attributes.type = 'texture';
+texture_cell{4}.Attributes.filename = '.\assets\A380\A380_L.tga';
+
+texture_cell{5}.Attributes.name = 'A380_R';
+texture_cell{5}.Attributes.type = 'texture';
+texture_cell{5}.Attributes.filename = '.\assets\A380\A380_R.tga';
+
+texture_cell{6}.Attributes.name = 'A380_part1';
+texture_cell{6}.Attributes.type = 'texture';
+texture_cell{6}.Attributes.filename = '.\assets\A380\A380_part1.tga';
+
+texture_cell{7}.Attributes.name = 'A380_part2';
+texture_cell{7}.Attributes.type = 'texture';
+texture_cell{7}.Attributes.filename = '.\assets\A380\A380_part2.tga';
+
+texture_cell{8}.Attributes.name = 'A380_part3';
+texture_cell{8}.Attributes.type = 'texture';
+texture_cell{8}.Attributes.filename = '.\assets\A380\A380_part3.tga';
 
 scene_struct.scene.textures.texture = texture_cell;
 
@@ -23,7 +51,7 @@ scatterer_cell{1}.Attributes.type = 'nonabsorber';
 scene_struct.scene.scatterers.scatterer = scatterer_cell;
 
 %% Materials
-material_cell = cell(3, 1);
+material_cell = cell(14, 1);
 
 material_cell{1}.Attributes.name = 'air';
 material_cell{1}.Attributes.type = 'refractive';
@@ -39,11 +67,76 @@ material_cell{2}.Attributes.emission = 'black'; % can also be array
 material_cell{2}.Attributes.colour = 'grey1'; % can also be array
 material_cell{2}.Attributes.roughness = 1;
 
-material_cell{3}.Attributes.name = 'airplane_mat';
+material_cell{3}.Attributes.name = 'b737_800_2_t';
 material_cell{3}.Attributes.type = 'diffuse_tex';
 material_cell{3}.Attributes.emission = 'black'; % can also be array
-material_cell{3}.Attributes.texture = 'airplane_texture'; % can also be index
+material_cell{3}.Attributes.texture = 'b737_800_2_T'; % can also be index
 material_cell{3}.Attributes.roughness = 1;
+
+material_cell{4}.Attributes.name = 'matte__ffffffff01';
+material_cell{4}.Attributes.type = 'diffuse';
+material_cell{4}.Attributes.emission = 'black'; % can also be array
+material_cell{4}.Attributes.colour = [0.243137, 0.243137, 0.243137]; % can also be index
+material_cell{4}.Attributes.roughness = 1;
+
+material_cell{5}.Attributes.name = 'Material__57';
+material_cell{5}.Attributes.type = 'diffuse';
+material_cell{5}.Attributes.emission = 'black'; % can also be array
+material_cell{5}.Attributes.colour = [1, 1, 1]; % can also be index
+material_cell{5}.Attributes.roughness = 1;
+
+material_cell{6}.Attributes.name = 'black';
+material_cell{6}.Attributes.type = 'diffuse';
+material_cell{6}.Attributes.emission = 'black'; % can also be array
+material_cell{6}.Attributes.colour = [0.164706, 0.164706, 0.164706]; % can also be index
+material_cell{6}.Attributes.roughness = 1;
+
+material_cell{7}.Attributes.name = 'a380_01';
+material_cell{7}.Attributes.type = 'diffuse_tex';
+material_cell{7}.Attributes.emission = 'black'; % can also be array
+material_cell{7}.Attributes.texture = 'a380_01'; % can also be index
+material_cell{7}.Attributes.roughness = 1;
+
+material_cell{8}.Attributes.name = 'a380_mw';
+material_cell{8}.Attributes.type = 'diffuse_tex';
+material_cell{8}.Attributes.emission = 'black'; % can also be array
+material_cell{8}.Attributes.texture = 'A380_mw'; % can also be index
+material_cell{8}.Attributes.roughness = 1;
+
+material_cell{9}.Attributes.name = 'a380_l_';
+material_cell{9}.Attributes.type = 'diffuse_tex';
+material_cell{9}.Attributes.emission = 'black'; % can also be array
+material_cell{9}.Attributes.texture = 'A380_L'; % can also be index
+material_cell{9}.Attributes.roughness = 1;
+
+material_cell{10}.Attributes.name = 'a380_r';
+material_cell{10}.Attributes.type = 'diffuse_tex';
+material_cell{10}.Attributes.emission = 'black'; % can also be array
+material_cell{10}.Attributes.texture = 'A380_R'; % can also be index
+material_cell{10}.Attributes.roughness = 1;
+
+material_cell{11}.Attributes.name = 'a380_part1';
+material_cell{11}.Attributes.type = 'diffuse_tex';
+material_cell{11}.Attributes.emission = 'black'; % can also be array
+material_cell{11}.Attributes.texture = 'A380_part1'; % can also be index
+material_cell{11}.Attributes.roughness = 1;
+
+material_cell{12}.Attributes.name = 'a380_part2';
+material_cell{12}.Attributes.type = 'diffuse_tex';
+material_cell{12}.Attributes.emission = 'black'; % can also be array
+material_cell{12}.Attributes.texture = 'A380_part2'; % can also be index
+material_cell{12}.Attributes.roughness = 1;
+
+material_cell{13}.Attributes.name = 'a380_part3';
+material_cell{13}.Attributes.type = 'diffuse_tex';
+material_cell{13}.Attributes.emission = 'black'; % can also be array
+material_cell{13}.Attributes.texture = 'A380_part3'; % can also be index
+material_cell{13}.Attributes.roughness = 1;
+
+material_cell{14}.Attributes.name = 'airplane_mat';
+material_cell{14}.Attributes.type = 'aggregate';
+material_cell{14}.Attributes.materials_names = 'b737_800_2_t; b737_800_2_t0; matte__ffffffff01; Material__57; b737_800_2_t1; matte__ffffffff010; Material__68; black; black0; a380_01; Material__46; black1; a380_010; a380_mw; a380_l_; a380_r; a380_part1; a380_part2; a380_part3; Material__35';
+material_cell{14}.Attributes.materials_list = 'b737_800_2_t; b737_800_2_t; matte__ffffffff01; Material__57; b737_800_2_t; matte__ffffffff01; Material__57; black; black; a380_01; black; black; a380_01; a380_mw; a380_l_; a380_r; a380_part1; a380_part2; a380_part3; black'; % can also be array
 
 scene_struct.scene.materials.material = material_cell;
 
@@ -54,7 +147,7 @@ mesh_geometry_cell = cell(1, 1);
 
 mesh_geometry_cell{1}.Attributes.name = 'airplane_mesh';
 mesh_geometry_cell{1}.Attributes.type = 'mesh_geometry';
-mesh_geometry_cell{1}.Attributes.filename = '.\assets\A380.obj';
+mesh_geometry_cell{1}.Attributes.filename = '.\assets\A380\A380.obj';
 
 scene_struct.scene.mesh_geometries.mesh_geometry = mesh_geometry_cell;
 
