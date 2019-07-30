f36affe985305e5bb4364518d50e678a7e377596
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-07-30 10:18:49 -0400

    upgraded map scene to new format

--- scenes/scene_map.m
+++ scenes/scene_map.m
@@ -1,65 +1,167 @@
+close all;
+clear all;
+clc;
+
 %% Scene
+scene_name = 'map';
+scene_struct.scene.Attributes.name = scene_name;
+scene_struct.scene.Attributes.primitive_list = 'ground, triangleearth1, triangleearth2';
 
-% Colours
-yellow = [0.98, 1, 0.9];
-purple = [0.98, 0.7, 0.85];
-green = [0.8, 0.95, 0.6];
-white = [1, 1, 1];
-black = [0, 0, 0];
-grey1 = [0.5, 0.5, 0.5];
-grey2 = [0.75, 0.75, 0.75];
-blue = [0.1, 0.4, 0.8];
-turquoise = [0.6, 0.95, 0.8];
-watercolour = [0.6, 0.9, 1];
-red = [0.9568627450980392, 0.2784313725490196, 0.2784313725490196];
-gemcolor = [0 0.9 1];
-orange = [1, 0.6039, 0];
-teal = [0.1529, 0.6549, 0.8471];
+%% Textures
 
-% General stuff
-neutralmatrix = transformmatrix(); % Should never be changed, used for triangles
-air = refractive(black, white, 1.001, struct('ind', 0), nonabsorber()); %%% CHECK generate_scene is 10x slower when putting [] as is_in, this is a workaround
+texture_cell = cell(0);
 
+texture_cell{end+1}.Attributes.name = 'earth_texture';
+texture_cell{end}.Attributes.type = 'texture';
+texture_cell{end}.Attributes.filename = 'assets\earth_2048.png';
 
-scenename = 'map';
+scene_struct.scene.textures.texture = texture_cell;
 
-fprintf('\nScene name: %s\n', scenename);
-fprintf('\nScene building\n');
-tic
+%% Scattering
+scatterer_cell = cell(0);
 
-% Materials
-difgreen = diffuse(black, green, 1);
-earthmat = diffuse_tex(black, texture('.\assets\earth_2048.png'), 1);
+scatterer_cell{end+1}.Attributes.name = 'air_absorber';
+scatterer_cell{end}.Attributes.type = 'nonabsorber';
 
-% Objects
-ground = sphere(difgreen, transformmatrix());
-ground.transformation.uniformscale(100);
-ground.transformation.translate([0, 0, -101]);
+scene_struct.scene.scatterers.scatterer = scatterer_cell;
 
-triangleearth1 = triangle(earthmat, [-1, 4, 1; -1, 4, -1; 1, 4, -1], [0, -1, 0; 0, -1, 0; 0, -1, 0], [0, 1; 0, 0; 1, 0], neutralmatrix);
-triangleearth2 = triangle(earthmat, [-1, 4, 1; 1, 4, -1; 1, 4, 1], [0, -1, 0; 0, -1, 0; 0, -1, 0], [0, 1; 1, 0; 1, 1], neutralmatrix);
+%% Materials
+material_cell = cell(0);
 
-ascene = scene(ground, triangleearth1, triangleearth2);
+material_cell{end+1}.Attributes.name = 'air';
+material_cell{end}.Attributes.type = 'transparent';
+material_cell{end}.Attributes.priority = 0;
+material_cell{end}.Attributes.scattering_fn = 'air_absorber'; % can be index or name
 
-toc
+material_cell{end+1}.Attributes.name = 'difgreen';
+material_cell{end}.Attributes.type = 'diffuse';
+material_cell{end}.Attributes.emission = 'black'; % can also be array
+material_cell{end}.Attributes.colour = 'green'; % can also be array
+material_cell{end}.Attributes.roughness = 1;
 
-fprintf('\nScene updating\n');
-tic
-ascene.update;
-toc
+material_cell{end+1}.Attributes.name = 'earthmat';
+material_cell{end}.Attributes.type = 'diffuse_tex';
+material_cell{end}.Attributes.emission = 'black'; % can also be array
+material_cell{end}.Attributes.texture = 'earth_texture';
+material_cell{end}.Attributes.roughness = 1; 
 
-fprintf('\nAcceleration structure building\n');
-tic
-ascene.buildacc;
-toc
+scene_struct.scene.materials.material = material_cell;
 
-%save scene.mat ascene
+%% Transform matrices
+
+%% Meshes
+
+%% Objects
+object_cell = cell(0);
+
+object_cell{end+1}.Attributes.name = 'ground';
+object_cell{end}.Attributes.type = 'sphere';
+object_cell{end}.Attributes.material = 'difgreen'; % can also be name
+object_cell{end}.Attributes.transform_matrix = NaN; % if not empty, search for right matrix, if empty, create.
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'translate';
+transformation_pre_cell{end}.Attributes.value = [0, 0, -101];
+transformation_pre_cell{end+1}.Attributes.type = 'uniformscale';
+transformation_pre_cell{end}.Attributes.value = 100;
+object_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
+
+object_cell{end+1}.Attributes.name = 'triangleearth1';
+object_cell{end}.Attributes.type = 'triangle';
+object_cell{end}.Attributes.material = 'earthmat'; % can also be name
+object_cell{end}.Attributes.points = [-1, 4, 1; -1, 4, -1; 1, 4, -1];
+object_cell{end}.Attributes.normals = [0, -1, 0; 0, -1, 0; 0, -1, 0];
+object_cell{end}.Attributes.texture_coordinates = [0, 1; 0, 0; 1, 0];
+object_cell{end}.Attributes.transform_matrix = NaN; % if not empty, search for right matrix, if empty, create.
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'rotatezaxis';
+transformation_pre_cell{end}.Attributes.value = 0;
+object_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
+
+object_cell{end+1}.Attributes.name = 'triangleearth2';
+object_cell{end}.Attributes.type = 'triangle';
+object_cell{end}.Attributes.material = 'earthmat'; % can also be name
+object_cell{end}.Attributes.points = [-1, 4, 1; 1, 4, -1; 1, 4, 1];
+object_cell{end}.Attributes.normals = [0, -1, 0; 0, -1, 0; 0, -1, 0];
+object_cell{end}.Attributes.texture_coordinates = [0, 1; 1, 0; 1, 1];
+object_cell{end}.Attributes.transform_matrix = NaN; % if not empty, search for right matrix, if empty, create.
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'rotatezaxis';
+transformation_pre_cell{end}.Attributes.value = 0;
+object_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
+
+scene_struct.scene.objects.object = object_cell;
+
+%% Directional lights
+directional_light_cell = cell(0);
+
+directional_light_cell{end+1}.Attributes.name = 'sun';
+directional_light_cell{end}.Attributes.colour = [2.5, 2.5, 2] * 2;
+directional_light_cell{end}.Attributes.transform_matrix = NaN;
+directional_light_cell{end}.Attributes.type = 'directional_light';
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'uniformscale';
+transformation_pre_cell{end}.Attributes.value = 0.95;
+transformation_pre_cell{end+1}.Attributes.type = 'rotatez';
+transformation_pre_cell{end}.Attributes.value = -pi/4;
+transformation_pre_cell{end+1}.Attributes.type = 'rotatex';
+transformation_pre_cell{end}.Attributes.value = -3 * pi/8;
+directional_light_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
+
+scene_struct.scene.directional_lights.directional_light = directional_light_cell;
+
+%% Skyboxes
+skybox_cell = cell(0);
+
+skybox_cell{end+1}.Attributes.name = 'sky';
+skybox_cell{end}.Attributes.type = 'skybox_flat_sun';
+skybox_cell{end}.Attributes.colour = [0.75, 0.85, 1];
+skybox_cell{end}.Attributes.lights = 'sun'; % can be array of indices, or cell array of names. Maybe should be another struct?
+
+scene_struct.scene.skyboxes.skybox = skybox_cell;
+
+%% Image buffers
+res_x = 1800;
+res_y = 1200;
+imgbuffer_cell = cell(0);
+
+imgbuffer_cell{end+1}.Attributes.name = 'buffer1';
+imgbuffer_cell{end}.Attributes.type = 'imgbuffer_opengl';
+imgbuffer_cell{end}.Attributes.resx = res_x;
+imgbuffer_cell{end}.Attributes.resy = res_y;
+
+scene_struct.scene.imgbuffers.imgbuffer = imgbuffer_cell;
 
 %% Camera
+aspect_ratio = res_x/res_y;
+fov(2) = 80 * pi/180;
+fov(1) = fov(2)/aspect_ratio;
 
-%camera.transformation.rotatez(-pi/6);
-%camera.transformation.translate([-1, -2, 0]);
-%camera.transformation.rotatex(-pi/8);
+camera_cell = cell(0);
+camera_cell{end+1}.Attributes.name = 'camera1';
+camera_cell{end}.Attributes.type = 'cam_aperture';
+camera_cell{end}.Attributes.transform_matrix = NaN;
+camera_cell{end}.Attributes.filename = NaN; % if is empty, use next available with scene name
+upvector = [0, 0, 1];
+camera_cell{end}.Attributes.up = upvector/norm(upvector);
+camera_cell{end}.Attributes.fov = fov;
+camera_cell{end}.Attributes.subpix = [1, 1];
+camera_cell{end}.Attributes.imgbuffer = 'buffer1'; % can be index or name
+camera_cell{end}.Attributes.medium_list = 'air, air'; % can be index or name
+camera_cell{end}.Attributes.skybox = 'sky'; % can be index or name
+camera_cell{end}.Attributes.max_bounces = 8;
+camera_cell{end}.Attributes.focal_length = 2;
+camera_cell{end}.Attributes.aperture = 0.0;
+camera_cell{end}.Attributes.gammaind = 1;
+camera_cell{end}.Attributes.rendermode = 'accumulation';
+camera_cell{end}.Attributes.n_iter = inf;
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'translate';
+transformation_pre_cell{end}.Attributes.value = [0, 0, 0];
+transformation_pre_cell{end+1}.Attributes.type = 'rotatezaxis';
+transformation_pre_cell{end}.Attributes.value = 0;
+camera_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
 
-camera.update;
-camera.update; % second time to kill blur
+scene_struct.scene.cameras.camera = camera_cell;
+
+%% Output
+struct2xml(scene_struct, ['.', filesep, 'scenes', filesep, scene_name, '.xml']);
