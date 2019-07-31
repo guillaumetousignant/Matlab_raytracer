ec89392eb900c12492260231811a74e8f767c51d
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-07-31 10:29:41 -0400

    upgraded portals scene

--- scenes/scene_portals.m
+++ scenes/scene_portals.m
@@ -1,79 +1,203 @@
+close all;
+clear all;
+clc;
+
 %% Scene
+scene_name = 'portals';
+scene_struct.scene.Attributes.name = scene_name;
+scene_struct.scene.Attributes.primitive_list = 'portal1, portal2, planegrey1, planegrey2';
 
-% Colours
-yellow = [0.98, 1, 0.9];
-purple = [0.98, 0.7, 0.85];
-green = [0.8, 0.95, 0.6];
-white = [1, 1, 1];
-black = [0, 0, 0];
-grey1 = [0.5, 0.5, 0.5];
-grey2 = [0.75, 0.75, 0.75];
-blue = [0.1, 0.4, 0.8];
-turquoise = [0.6, 0.95, 0.8];
-watercolour = [0.6, 0.9, 1];
-red = [0.9568627450980392, 0.2784313725490196, 0.2784313725490196];
-gemcolor = [0 0.9 1];
-orange = [1, 0.6039, 0];
-teal = [0.1529, 0.6549, 0.8471];
+%% Textures
+texture_cell = cell(0);
 
-% General stuff
-neutralmatrix = transformmatrix(); % Should never be changed, used for triangles
-air = refractive(black, white, 1.001, struct('ind', 0), nonabsorber()); %%% CHECK generate_scene is 10x slower when putting [] as is_in, this is a workaround
+texture_cell{end+1}.Attributes.name = 'background';
+texture_cell{end}.Attributes.type = 'texture';
+texture_cell{end}.Attributes.filename = 'assets\Ocean from horn.jpg';
 
+scene_struct.scene.textures.texture = texture_cell;
 
-scenename = 'portals';
+%% Scattering
+scatterer_cell = cell(0);
 
-fprintf('\nScene name: %s\n', scenename);
-fprintf('\nScene building\n');
-tic
+scatterer_cell{end+1}.Attributes.name = 'air_absorber';
+scatterer_cell{end}.Attributes.type = 'nonabsorber';
 
-% Materials
-difgrey = diffuse(black, grey1, 1);
-portalmat11 = portal(transformmatrix(), air);
-portalmat21 = portal(transformmatrix(), air);
-portalmat11.transformation.rotatez(pi);
-portalmat11.transformation.translate([0 -3 0]);
-portalmat21.transformation.translate([0 3 0]);
-portalmat21.transformation.rotatez(pi);
-diffuseorange = diffuse(orange, white, 1);
-diffuseteal = diffuse(teal, white, 1);
-portalmat = fresnelmix_in(portalmat11, diffuseorange, 1.5);
-portalmat2 = fresnelmix_in(portalmat21, diffuseteal, 1.5);
+scene_struct.scene.scatterers.scatterer = scatterer_cell;
 
-% Objects 
-planegrey1 = triangle(difgrey, [-2, 4, -0.5; -2, 0, -0.5; 2, 0, -0.5], [], [], neutralmatrix);
-planegrey2 = triangle(difgrey, [-2, 4, -0.5; 2, 0, -0.5; 2, 4, -0.5], [], [], neutralmatrix);
+%% Materials
+material_cell = cell(0);
 
-portalsphere = sphere(portalmat, transformmatrix());
-portalsphere.transformation.uniformscale(0.75);
-portalsphere.transformation.translate([0 0 0]);
+material_cell{end+1}.Attributes.name = 'air';
+material_cell{end}.Attributes.type = 'transparent';
+material_cell{end}.Attributes.priority = 0;
+material_cell{end}.Attributes.scattering_fn = 'air_absorber'; % can be index or name
 
-portalsphere2 = sphere(portalmat2, transformmatrix());
-portalsphere2.transformation.uniformscale(0.77);
-portalsphere2.transformation.translate([0 -3 0]);
+material_cell{end+1}.Attributes.name = 'difgrey';
+material_cell{end}.Attributes.type = 'diffuse';
+material_cell{end}.Attributes.emission = 'black'; % can also be array
+material_cell{end}.Attributes.colour = 'grey1'; % can also be array
+material_cell{end}.Attributes.roughness = 1;
 
-%ascene = scene(planegrey1, planegrey2, portalsphere);
-ascene = scene(portalsphere, portalsphere2);
+material_cell{end+1}.Attributes.name = 'difteal';
+material_cell{end}.Attributes.type = 'diffuse';
+material_cell{end}.Attributes.emission = 'teal'; % can also be array
+material_cell{end}.Attributes.colour = 'white'; % can also be array
+material_cell{end}.Attributes.roughness = 0.1;
 
-toc
+material_cell{end+1}.Attributes.name = 'diforange';
+material_cell{end}.Attributes.type = 'diffuse';
+material_cell{end}.Attributes.emission = 'orange'; % can also be array
+material_cell{end}.Attributes.colour = 'white'; % can also be array
+material_cell{end}.Attributes.roughness = 0.1;
 
-fprintf('\nScene updating\n');
-tic
-ascene.update;
-toc
+material_cell{end+1}.Attributes.name = 'portalmat11';
+material_cell{end}.Attributes.type = 'portal';
+material_cell{end}.Attributes.medium_list = 'air, air';
+material_cell{end}.Attributes.transform_matrix = NaN;
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'rotatez';
+transformation_pre_cell{end}.Attributes.value = pi;
+transformation_pre_cell{end+1}.Attributes.type = 'translate';
+transformation_pre_cell{end}.Attributes.value = [0, -3, 0];
+material_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
 
-fprintf('\nAcceleration structure building\n');
-tic
-ascene.buildacc;
-toc
+material_cell{end+1}.Attributes.name = 'portalmat12';
+material_cell{end}.Attributes.type = 'portal';
+material_cell{end}.Attributes.medium_list = 'air, air';
+material_cell{end}.Attributes.transform_matrix = NaN;
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'rotatez';
+transformation_pre_cell{end}.Attributes.value = pi;
+transformation_pre_cell{end+1}.Attributes.type = 'translate';
+transformation_pre_cell{end}.Attributes.value = [0, 3, 0];
+material_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
 
-%save scene.mat ascene
+material_cell{end+1}.Attributes.name = 'portalmat1';
+material_cell{end}.Attributes.type = 'fresnelmix';
+material_cell{end}.Attributes.material_refracted = 'portalmat11'; % can also be array
+material_cell{end}.Attributes.material_reflected = 'diforange'; % can also be array
+material_cell{end}.Attributes.ind = 1.5;
+
+material_cell{end+1}.Attributes.name = 'portalmat2';
+material_cell{end}.Attributes.type = 'fresnelmix';
+material_cell{end}.Attributes.material_refracted = 'portalmat12'; % can also be array
+material_cell{end}.Attributes.material_reflected = 'difteal'; % can also be array
+material_cell{end}.Attributes.ind = 1.5;
+
+scene_struct.scene.materials.material = material_cell;
+
+%% Transform matrices
+
+%% Meshes
+
+%% Objects
+object_cell = cell(0);
+
+object_cell{end+1}.Attributes.name = 'portal1';
+object_cell{end}.Attributes.type = 'sphere';
+object_cell{end}.Attributes.material = 'portalmat1'; % can also be name
+object_cell{end}.Attributes.transform_matrix = NaN; % if not empty, search for right matrix, if empty, create.
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'translate';
+transformation_pre_cell{end}.Attributes.value = [0, 0, 0];
+transformation_pre_cell{end+1}.Attributes.type = 'uniformscale';
+transformation_pre_cell{end}.Attributes.value = 0.75;
+object_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
+
+object_cell{end+1}.Attributes.name = 'portal2';
+object_cell{end}.Attributes.type = 'sphere';
+object_cell{end}.Attributes.material = 'portalmat2'; % can also be name
+object_cell{end}.Attributes.transform_matrix = NaN; % if not empty, search for right matrix, if empty, create.
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'translate';
+transformation_pre_cell{end}.Attributes.value = [0, -3, 0];
+transformation_pre_cell{end+1}.Attributes.type = 'uniformscale';
+transformation_pre_cell{end}.Attributes.value = 0.77;
+object_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
+
+object_cell{end+1}.Attributes.name = 'planegrey1';
+object_cell{end}.Attributes.type = 'triangle';
+object_cell{end}.Attributes.material = 'difgrey'; % can also be name
+object_cell{end}.Attributes.points = [-2, 2, -0.5; -2, -2, -0.5; 2, -2, -0.5];
+object_cell{end}.Attributes.normals = NaN;
+object_cell{end}.Attributes.texture_coordinates = NaN;
+object_cell{end}.Attributes.transform_matrix = NaN; % if not empty, search for right matrix, if empty, create.
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'rotatezaxis';
+transformation_pre_cell{end}.Attributes.value = 0;
+object_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
+
+object_cell{end+1}.Attributes.name = 'planegrey2';
+object_cell{end}.Attributes.type = 'triangle';
+object_cell{end}.Attributes.material = 'difgrey'; % can also be name
+object_cell{end}.Attributes.points = [-2, 2, -0.5; 2, -2, -0.5; 2, 2, -0.5];
+object_cell{end}.Attributes.normals = NaN;
+object_cell{end}.Attributes.texture_coordinates = NaN;
+object_cell{end}.Attributes.transform_matrix = NaN; % if not empty, search for right matrix, if empty, create.
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'rotatezaxis';
+transformation_pre_cell{end}.Attributes.value = 0;
+object_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
+
+scene_struct.scene.objects.object = object_cell;
+
+%% Directional lights
+
+%% Skyboxes
+skybox_cell = cell(0);
+skybox_cell{end+1}.Attributes.name = 'sky';
+skybox_cell{end}.Attributes.type = 'skybox_texture_sun';
+skybox_cell{end}.Attributes.light_position = [0.6209296, 1-0.2292542];
+skybox_cell{end}.Attributes.texture = 'background';
+skybox_cell{end}.Attributes.light_colour = [0.996, 0.941, 0.918] * 1.586010 * 8;
+skybox_cell{end}.Attributes.light_radius = 0.035;
+
+scene_struct.scene.skyboxes.skybox = skybox_cell;
+
+%% Image buffers
+res_x = 1800;
+res_y = 1200;
+imgbuffer_cell = cell(0);
+
+imgbuffer_cell{end+1}.Attributes.name = 'buffer1';
+imgbuffer_cell{end}.Attributes.type = 'imgbuffer_opengl';
+imgbuffer_cell{end}.Attributes.resx = res_x;
+imgbuffer_cell{end}.Attributes.resy = res_y;
+
+scene_struct.scene.imgbuffers.imgbuffer = imgbuffer_cell;
 
 %% Camera
+aspect_ratio = res_x/res_y;
+fov(2) = 80 * pi/180;
+fov(1) = fov(2)/aspect_ratio;
 
-%camera.transformation.rotatez(-pi/6);
-%camera.transformation.translate([-1, -2, 0]);
-%camera.transformation.rotatex(-pi/8);
+camera_cell = cell(0);
+camera_cell{end+1}.Attributes.name = 'camera1';
+camera_cell{end}.Attributes.type = 'cam_aperture';
+camera_cell{end}.Attributes.transform_matrix = NaN;
+camera_cell{end}.Attributes.filename = NaN; % if is empty, use next available with scene name
+upvector = [0, 0, 1];
+camera_cell{end}.Attributes.up = upvector/norm(upvector);
+camera_cell{end}.Attributes.fov = fov;
+camera_cell{end}.Attributes.subpix = [1, 1];
+camera_cell{end}.Attributes.imgbuffer = 'buffer1'; % can be index or name
+camera_cell{end}.Attributes.medium_list = 'air, air'; % can be index or name
+camera_cell{end}.Attributes.skybox = 'sky'; % can be index or name
+camera_cell{end}.Attributes.max_bounces = 16;
+camera_cell{end}.Attributes.focal_length = 2;
+camera_cell{end}.Attributes.aperture = 0.01;
+camera_cell{end}.Attributes.time = [0, 1];
+camera_cell{end}.Attributes.gammaind = 1;
+camera_cell{end}.Attributes.rendermode = 'accumulation';
+camera_cell{end}.Attributes.n_iter = inf;
+transformation_pre_cell = cell(0);
+transformation_pre_cell{end+1}.Attributes.type = 'translate';
+transformation_pre_cell{end}.Attributes.value = [0, -2, 0];
+transformation_pre_cell{end+1}.Attributes.type = 'rotatezaxis';
+transformation_pre_cell{end}.Attributes.value = 0;
+camera_cell{end}.transformations_pre.transformation_pre = transformation_pre_cell;
 
-camera.update;
-camera.update; % second time to kill blur
+scene_struct.scene.cameras.camera = camera_cell;
+
+%% Output
+struct2xml(scene_struct, ['.', filesep, 'scenes', filesep, scene_name, '.xml']);
